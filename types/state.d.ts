6d35e2f4d2afba86aebbfe7383ee8b11d9cc21ff
Jeff Mendez <jeffmendez19@gmail.com>
2020-09-27 07:23:20 -0400

    chore: add types to ignore

--- types/state.d.ts
+++
@@ -1,9 +0,0 @@
-import React from "react";
-declare type Props = {
-    children: React.ReactNode;
-    defaultState?: {};
-    reducer?: any;
-};
-export declare const useStateValue: () => {};
-export declare const StateProvider: ({ children, defaultState, reducer, }: Props) => JSX.Element;
-export {};
